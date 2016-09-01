ccb1935653d6b971f08d430cef8de96a446467d3
welder.souza <welder.souza@cnj.jus.br>
2016-09-01 14:28:54 -0300

    Adicionando botões

--- app/src/main/java/br/edu/welder/aulacinco/MainActivity.java
+++ app/src/main/java/br/edu/welder/aulacinco/MainActivity.java
@@ -2,13 +2,24 @@ package br.edu.welder.aulacinco;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button enviar;
+    private TextView mensagemIn;
+    private TextView mensagemOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        enviar = (Button) findViewById(R.id.app_button);
+        mensagemIn = (TextView) findViewById(R.id.editTexto);
+        mensagemOut = (TextView) findViewById(R.id.textShowMessage);
+
 
     }
 }
